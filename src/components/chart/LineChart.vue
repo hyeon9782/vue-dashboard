f8900364fe85237dd596577ee86e34e42060f6c0
hyeon9782 <hyeon9782@gmail.com>
2024-04-09 21:40:07 +0900

    feat: LineChart props로 데이터 받도록 리팩토링

--- src/components/chart/LineChart.vue
+++ src/components/chart/LineChart.vue
@@ -4,8 +4,17 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, toRefs } from "vue";
 import Highcharts from "highcharts";
+
+const props = defineProps({
+  series: [],
+  xAxis: {},
+  yAxis: {},
+});
+
+const { series, xAxis, yAxis } = toRefs(props);
+
 const chartOptions = ref({
   chart: {
     type: "line",
@@ -13,35 +22,9 @@ const chartOptions = ref({
   title: {
     text: "라인 차트",
   },
-  xAxis: {
-    categories: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
-  },
-  yAxis: {
-    title: {
-      text: "온도 (℃)",
-    },
-  },
-  series: [
-    {
-      name: "온도",
-      data: [
-        7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,
-      ],
-    },
-  ],
+  xAxis,
+  yAxis,
+  series,
 });
 
 onMounted(() => {
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,21 @@
 <template>
   <main class="page">
-    Home
-    <LineChart />
+    <LineChart :chart="chart" :xAxis="xAxis" :yAxis="yAxis" :series="series" />
   </main>
 </template>
 <script lang="ts" setup>
 import LineChart from "@/components/chart/LineChart.vue";
+import { ref } from "vue";
+
+const chart = ref({});
+const xAxis = ref({});
+const yAxis = ref({});
+const series = ref([
+  {
+    name: "온도",
+    data: [87.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+  },
+]);
 </script>
 <style lang="scss" scoped>
 .page {
