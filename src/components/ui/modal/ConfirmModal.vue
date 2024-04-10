b5d0f60a0bc5fa65e7c03ba17d89683248db08a3
hyeon9782 <hyeon9782@gmail.com>
2024-04-10 09:42:58 +0900

    feat: Modal, ConfirmModal 컴포넌트 구현

---
+++ src/components/ui/modal/ConfirmModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <Modal>
+    <div class="modal-content">
+      <h1>{{ props.title }}</h1>
+      <h2>{{ props.content }}</h2>
+      <button @click="props.onClose">취소</button>
+      <button @click="props.onSubmit">확인</button>
+    </div>
+  </Modal>
+</template>
+<script lang="ts" setup>
+import Modal from "@components/ui/modal/Modal.vue";
+const props = defineProps({
+  onClose: Function,
+  onSubmit: Function,
+  title: String,
+  content: String,
+});
+</script>
+<style lang="scss">
+.modal-content {
+  width: 300px;
+  margin: auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+</style>
---
+++ src/components/ui/modal/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <Teleport to="body">
+    <Transition name="modal">
+      <div class="modal" v-show="isOpen">
+        <slot></slot>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  isOpen: Boolean,
+});
+</script>
+<style lang="scss" scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
