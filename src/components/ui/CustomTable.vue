7217b8481a887e1ef1804f8d3d2b33f061db9004
hyeon9782 <hyeon9782@gmail.com>
2024-04-14 18:21:29 +0900

    feat: Table 컴포넌트 기능구현

--- src/components/ui/CustomTable.vue
+++ src/components/ui/CustomTable.vue
@@ -1,7 +1,23 @@
 <template>
   <table>
-    <slot></slot>
+    <thead>
+      <tr v-for="head in thead" :key="head.id">
+        <th v-for="td in head" :key="td.id">
+          {{ td.item }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+      </tr>
+    </tbody>
   </table>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps({
+  thead: [],
+  tbody: [],
+});
+</script>
 <style lang=""></style>
